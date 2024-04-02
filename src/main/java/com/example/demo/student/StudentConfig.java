2db36407c056cf3428d3c6578346bafe0bd22ef3
Terry Tosh <tmt103@txstate.edu>
2024-04-02 12:55:23 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student terry = new Student(
+                    "Terry",
+                    "terry@gmail.com",
+                    LocalDate.of(2000, JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, JANUARY, 5)
+            );
+
+            repository.saveAll(List.of(terry, alex));
+
+        };
+    }
+
+}
